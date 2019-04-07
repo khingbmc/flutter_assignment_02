d71977cc83a98c7cf0b3f6da1e65b873ff97a30e
khingbmc <ratanon.jan@gmail.com>
2019-04-07 19:30:40 +0700

    fix todo screen and add current screen state

--- lib/ui/todo_screen.dart
+++ lib/ui/todo_screen.dart
@@ -1,14 +1,6 @@
-import 'package:flutter_assignment_02/Database/DBHelper.dart';
-import 'package:flutter_assignment_02/Models/todo.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_assignment_02/ui/add_screen.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
-
-Future<List<Todo>> getTodoListFromDB() async{
-    var dbHelper = DBHelper();
-    Future<List<Todo>> todos = dbHelper.getAllTodo();
-  }
+import './add_screen.dart';
+import 'package:flutter_assignment_02/Models/todo.dart';
 
 class TodoScreen extends StatefulWidget{
   @override
@@ -19,81 +11,146 @@ class TodoScreen extends StatefulWidget{
 
 }
 
-
-
-class TodoScreenState extends State<TodoScreen> {
-  final controller_title = new TextEditingController();
-  int _current_state = 0;
-  int countTodo = 0;
+class TodoScreenState extends State{
+  
+  int _curState = 0;
+  int countList = 0;
   int countDone = 0;
-  Todo todo = new Todo();
+  List<Todo> todos;
+  List<Todo> todosDone;
 
-  String title;
-  int done;
+  TodoProvide db = TodoProvide();
 
-  final scaffoldKey = new GlobalKey<ScaffoldState>();
-  final formKey = new GlobalKey<FormState>();
-
+  void getTodos() async{
+    await db.open("todo.db");
+    db.getAllTodos().then((todos){
+      setState(() {
+              this.countList = todos.length;
+              this.todos = todos;
+            });
+    });
+    db.getAllDone().then((todosDone){
+      setState(() {
+              this.countDone = todosDone.length;
+              this.todosDone = todosDone;
+            });
+    });
+  }
+  
   
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      key: scaffoldKey,
-      appBar: new AppBar(
-        title: Text("Todo"),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: (){
-              addTodoList();
-              },
-          )
-        ],
+    // TODO: implement build
+    List current_tab = <Widget>[
+      IconButton(
+        icon: Icon(Icons.add),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => TodoAdd()));
+        },
       ),
-      body: new Container(
-        padding: EdgeInsets.all(16.0),
-        child: FutureBuilder<List<Todo>>(
-          future: getTodoListFromDB(),
-          builder: (context, snapshot) {
-            if(snapshot.data != null) {
-              if(snapshot.hasData){
-                countTodo = snapshot.data.length;
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index){
-                    if(snapshot.data[index].done == 0){
-                      return new Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
-                            Container(
-                              padding: const EdgeInsets.only(bottom: 8.0),
-                              child: Text(
-                                snapshot.data[index].title,
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            )
+      IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: (){
+          db.deleteAllDone();
+        },
+      )
+    ];
 
-                          ],),
-                        )
-                      ],
-                    );
-                    }
+  //change tab (task, complete) => store in list current screen
+    List current_screen = [
+      countList == 0 ? Text("No data found.."):
+      ListView.builder(
+        itemCount: countList,
+        itemBuilder: (context, int index){
+          return Column(
+            children: <Widget>[
+              Divider(height: 7.0,),
+              ListTile(
+                title: Text(this.todos[index].title,
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  fontSize: 24,
+                ),),
+                trailing: Checkbox(
+                  onChanged: (bool val){
+                    setState(() {
+                      todos[index].done = val;
+                    });
+                    db.update(todos[index]);
                   },
-                );
-              }
-            }
+                  value: todos[index].done,
+                ),
+              )
+            ],
+          );
+        },
+      ),
+      countDone == 0 ? Text("No data found.."):
+      ListView.builder(
+        itemCount: countDone,
+        itemBuilder: (context, int index){
+          return Column(
+            children: <Widget>[
+              Divider(height: 7.0,),
+              ListTile(
+                title: Text(this.todosDone[index].title,
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  fontSize: 24,
+                  ),
+                ),
+                trailing: Checkbox(
+                  onChanged: (bool val){
+                    setState(() {
+                      todosDone[index].done = val;
+                    });
+                    db.update(todosDone[index]);
+                  },
+                  value: todosDone[index].done,
+                ),
+
+              )
+            ],
+          );
+        },
+      )
+    ];
+
+    getTodos();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Todo"),
+          actions: <Widget>[
+            _curState == 0 ? current_tab[0] : current_tab[1], 
+
+          ],
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: _curState == 0 ? current_screen[0] : current_screen[1],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _curState,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text('Task')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.done_all),
+              title: Text('Completed')
+            )
+          ],
+          onTap: (int index){
+            setState(() {
+              _curState = index;
+            });
           },
         ),
       ),
     );
   }
 
-  void addTodoList() {
-  // add Todo list
-  Navigator.push(context, new MaterialPageRoute(builder: (context) => new TodoAdd()));
-  }
-
-  
-  
 }
